8447e5d607fe69285936103684e046ad3661e2ff
Gal <galsela2022@gmail.com>
2022-03-14 12:34:12 +0200

    add signup & login component and user model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginFormComponent } from './Auth/login-form/login-form.component';
+import { SignUpFormComponent } from './Auth/sign-up-form/sign-up-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    SignUpFormComponent
   ],
   imports: [
     BrowserModule,
