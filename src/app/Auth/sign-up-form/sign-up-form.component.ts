d102ca15181a92dca22d50707ac20a9129fa6409
Gal <galsela2022@gmail.com>
2022-03-14 13:53:51 +0200

    add injection authService

--- src/app/Auth/sign-up-form/sign-up-form.component.ts
+++ src/app/Auth/sign-up-form/sign-up-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService:AuthService) { }
 
   ngOnInit(): void {
   }
