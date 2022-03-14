60603a1923c59a57bfd26476bb9430436588a72f
ofir <ofir.sela26@gmail.com>
2022-03-14 20:18:54 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './Auth/Components/login-form/login-form.component';
+import { SignUpFormComponent } from './Auth/Components/sign-up-form/sign-up-form.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: 'main', component: MainScreenComponent },
+  {path: 'login', component: LoginFormComponent },
+  {path: 'sign-up', component: SignUpFormComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
